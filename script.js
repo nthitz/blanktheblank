6d670c43e2ca13b83a362da5d192a1dbdccd6176
Nick Yahnke <nthitz@gmail.com>
2015-11-28 14:37:04 -0800

    break up index.html

---
+++ script.js
@@ -0,0 +1,65 @@
+
+var verbs = ['hack', 'own', 'sell', 'buy', 'destroy', 'save', 'override',
+  'datamine', 'surveil', 'monitor', 'countersurveil', 'subvert', 'study',
+  'oppress', 'suppress', 'enslave', 'consume', 'censor', 'contain', 'subdue',
+  'conceal', 'conquer', 'silence', 'download', 'create', 'bailout', 'wipeout',
+  'control']
+
+var nouns = ['planet', 'network', 'internet', 'law', 'government', 'enemy',
+  'power core', 'infrastructure', 'airwaves', 'oceans', 'user data', 'future',
+  'past', 'moon', 'universe', 'metaverse', 'game', 'media', 'poor', 'rich',
+  'banks', 'lawyers', 'sheep', 'privileged', 'disadvantaged', 'people',
+  'civilians', 'consciousness', 'consensus']
+
+var text = null
+var animateTimeout = null
+function makeSentence() {
+  return chooseRandom(verbs) + '\nthe\n' + chooseRandom(nouns)
+}
+
+function chooseRandom(array) {
+  return array[~~(Math.random() * array.length)]
+}
+
+
+function init() {
+  text = d3.select('body').append('div').attr('class','text')
+  d3.select('body').on('click', animate)
+  animate()
+}
+
+
+function animate() {
+  clearTimeout(animateTimeout)
+  var sentence = makeSentence()
+  var duration = sentence.length * 100
+  var delay = 1000
+  text.datum(sentence)
+    .transition()
+    .delay(delay)
+    .duration(duration)
+    .ease(d3.ease('ease-out'))
+    .tween('text', typeWriterTween)
+    .each('end', function() {
+      text
+        .transition()
+        .delay(delay)
+        .duration(sentence.length * 50)
+        .tween('text', deleteTextTween)
+        .each('end', animate)
+    })
+}
+
+function typeWriterTween(d, i) {
+  return function (t) {
+    this.textContent = d.slice(0, Math.round( t * d.length) );
+  };
+}
+function deleteTextTween(d, i) {
+  return function (t) {
+    this.textContent = d.slice(0, Math.round( (1 - t) * d.length) );
+  };
+}
+
+
+init()
